5c5465613a049090bf77dd12f741d5b294471624
IshanX111 <51012063+IshanX111@users.noreply.github.com>
2020-01-03 03:29:28 +0600

    Add files via upload

---
+++ 113.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    double n,p,k;
+        while(scanf("%lf%lf",&n,&p)!=EOF){
+
+        k=pow(p,1.0/n);
+        printf("%.0lf\n",k);
+
+        }
+
+}
