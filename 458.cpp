7fcb078f37eaeee1acd617b82ed7750fa2906bd1
IshanX111 <51012063+IshanX111@users.noreply.github.com>
2020-01-22 23:57:18 +0600

    Add files via upload

---
+++ 458.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+        int i;
+        char a[1000];
+        while(gets(a)){
+        for(i=0;a[i]!='\0';i++){
+            printf("%c",a[i]-7);
+        }
+        printf("\n");
+        }
+
+}
