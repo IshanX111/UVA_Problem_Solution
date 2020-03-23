3b3be280327648a8ee6b390a58bf85a8a5f25cee
IshanX111 <51012063+IshanX111@users.noreply.github.com>
2020-03-23 13:48:41 +0600

    Add files via upload

---
+++ 10055.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    long long int a,b;
+    while(scanf("%lld%lld",&a,&b)!=EOF)
+    {
+        if(b>a)
+            printf("%lld\n",b-a);
+        else
+            printf("%lld\n",a-b);
+    }
+
+}
