3cf71d8335134097a653eac5800a388e2fc50267
Tajrian Islam Ishan <51012063+IshanX111@users.noreply.github.com>
2020-04-22 20:00:01 +0600

    Add files via upload

---
+++ 10469.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+
+    long long int a,b,res;
+    while(scanf("%lld%lld",&a,&b)!=EOF){
+        res=a^b;
+        cout<<res<<endl;
+    }
+
+}
